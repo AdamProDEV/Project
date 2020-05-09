b53fbad09f2ec47b941821e8c5bd64b5c53c4270
Sapirgil10 <61757780+Sapirgil10@users.noreply.github.com>
2020-05-09 13:44:07 +0300

    Update

--- src/test/java/SanityTests/LoginPage.java
+++ src/test/java/SanityTests/LoginPage.java
@@ -7,12 +7,12 @@ import WorkFlows.NewSign;
 public class LoginPage extends CommonOps {
 
     @Test
-    public void test01_FirstRegistration(){
+    public void test01_FirstRegistration() {
         NewSign.CreateAnAccount("Sapir11@gmail.com");
     }
 
     @Test
-    public void test02_PersonalInformationPage(){
+    public void test02_PersonalInformationPage() {
         NewSign.informationPage("Sapir", "Abargil", "12345678910");
     }
 }
